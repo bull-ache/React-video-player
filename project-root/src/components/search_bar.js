68185a746f0ac292af650681c94e768cf0329283
blakerscott <blakerscott@gmail.com>
2018-10-07 23:39:06 -0700

    add ability to manage state of search bar

--- project-root/src/components/search_bar.js
+++ project-root/src/components/search_bar.js
@@ -4,28 +4,20 @@ don't have to write React.Component each time. */
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-      /* Pass the event handler to the element that we
-      want to monitor for events. The event type 'Change' is
-      a vanilla HTML concept. This was refactored, previously looked
-      like this:
+  // initialize state with the constructor method
+  constructor(props) {
+    super(props);
 
-      return <input onChange={this.handleInputChange} />;
-    }
-
-    handleInputChange(event) {
-      console.log(event.target.value);
-      }
-    }   */
-
-      /* The event handler is a function that should
-    be ran whenever an event occurs.  An 'event' object must be passed in as an argument, although you can name it whatever you want. The event object provides information about
-    the event that occured. For our SearchBar component, we'll use the event object to get access to the value of the input,
-    i.e. the text that is added.*/
-
-    return <input onChange={event => console.log(event.target.value)} />;
+    this.state =  {term: ''};
   }
 
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />Value of the input: {this.state.term}
+      </div>
+    );
+  }
 }
 
 
