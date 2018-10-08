deaaa1212a4a0b26f7220311d9db83080224ecea
blakerscott <blakerscott@gmail.com>
2018-10-07 23:00:08 -0700

    refactored search bar to use an arrow function

--- project-root/src/components/search_bar.js
+++ project-root/src/components/search_bar.js
@@ -7,20 +7,27 @@ class SearchBar extends Component {
   render() {
       /* Pass the event handler to the element that we
       want to monitor for events. The event type 'Change' is
-      a vanilla HTML concept. */
-    return <input onChange={this.handleInputChange} />;
-  }
-  /* The event handler is a function that should
-    be ran whenever an event occurs.  An 'event' object must
-    be passed in as an argument, although you can name it
-    whatever you want. The event object provides information about
-    the event that occured. For our SearchBar component, we'll
-    use the event object to get access to the value of the input,
+      a vanilla HTML concept. This was refactored, previously looked
+      like this:
+
+      return <input onChange={this.handleInputChange} />;
+    }
+
+    handleInputChange(event) {
+      console.log(event.target.value);
+      }
+    }   */
+
+      /* The event handler is a function that should
+    be ran whenever an event occurs.  An 'event' object must be passed in as an argument, although you can name it whatever you want. The event object provides information about
+    the event that occured. For our SearchBar component, we'll use the event object to get access to the value of the input,
     i.e. the text that is added.*/
-  handleInputChange(event) {
-    console.log(event.target.value);
+
+    return <input onChange={(event) => console.log(event.target.value)} />;
   }
+
 }
+
 
 export default SearchBar;
 
