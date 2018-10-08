78a79a1d1a44dfc0c049dc41552d78b63542977c
blakerscott <blakerscott@gmail.com>
2018-10-07 22:47:58 -0700

    add event handler for search bar

--- project-root/src/components/search_bar.js
+++ project-root/src/components/search_bar.js
@@ -1,8 +1,24 @@
+/* This way of doing the import creates syntactic sugar, in that
+it breaks off the idea of 'Component' so that you
+don't have to write React.Component each time. */
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input />;
+      /* Pass the event handler to the element that we
+      want to monitor for events. The event type 'Change' is
+      a vanilla HTML concept. */
+    return <input onChange={this.handleInputChange} />;
+  }
+  /* The event handler is a function that should
+    be ran whenever an event occurs.  An 'event' object must
+    be passed in as an argument, although you can name it
+    whatever you want. The event object provides information about
+    the event that occured. For our SearchBar component, we'll
+    use the event object to get access to the value of the input,
+    i.e. the text that is added.*/
+  handleInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
